93f7c675b93c9408d9b2c1088b19d9f43fa97f41
RadX <ralobba@gmail.com>
2013-07-18 18:27:24 +0800

    Classes complete

---
+++ RandomPickerGenerator/RandomPickerGenerator/AddCustomRandomCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddCustomRandomCollectionViewController.h
+//  RandomPickerGenerator
+//
+//  Created by Rex Jason Alobba on 18/7/13.
+//  Copyright (c) 2013 ralobba@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddCustomRandomCollectionViewController : UIViewController
+
+@end
---
+++ RandomPickerGenerator/ResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsViewController.h
+//  RandomPickerGenerator
+//
+//  Created by Rex Jason Alobba on 18/7/13.
+//  Copyright (c) 2013 ralobba@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsViewController : UIViewController
+
+@end
