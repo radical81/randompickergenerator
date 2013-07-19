81d6bb0dee940b6683bd79e3aa9f49d58ba23875
Kenneth Poon <kenneth@allproperty.com.sg>
2013-07-19 13:54:47 +0800

    Add custom random collection

--- RandomPickerGenerator/RandomPickerGenerator/AddCustomRandomCollectionViewController.h
+++ RandomPickerGenerator/RandomPickerGenerator/AddCustomRandomCollectionViewController.h
@@ -9,5 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface AddCustomRandomCollectionViewController : UIViewController
+@property (retain, nonatomic) IBOutlet UIButton *addImagesButton;
+@property (retain, nonatomic) IBOutlet UILabel *imageAddedLabel;
+@property (retain, nonatomic) IBOutlet UITextField *nSelectionTextField;
+@property (retain, nonatomic) IBOutlet UISwitch *canRepeatSwitch;
+- (IBAction)onAddImagesButtonClicked:(id)sender;
 
 @end
